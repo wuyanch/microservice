bb7cc8969ae8f98cdcef764affa24e8759693bde
wuyanch <1046808671@qq.com>
2021-03-03 09:41:43 +0800

    init

---
+++ src/api/base.js
@@ -0,0 +1,2 @@
+const base = process.env.NODE_ENV==='production'?'':'' //服务器地址
+export default  base
--- src/main.js
+++ src/main.js
@@ -1,5 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
+//引入elementUI
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+
+//引入淘宝的自适应js
+import 'lib-flexible/flexible.js'
+
+//axios
+import axios from 'axios'
 
 Vue.config.productionTip = false
 
